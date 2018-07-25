a87d4b37b5676baf72ea82b41c1ae2b49aac0c7e
jeppeter <jeppeter@gmail.com>
2018-07-25 09:45:15 +0800

    to add mkefi.sh

---
+++ mkefi.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+findmods=$(find ./grub-core -name '*\.mod' -printf '%f ' | sed 's/\.mod / /g')
+echo "findmods [$findmods]"
+#./grub-mkimage -O x86_64-efi -d ./grub-core -o bootx64.efi -p "" $findmods
+./grub-mkimage -O x86_64-efi -d ./grub-core -o grubx64.efi -p "" part_gpt part_msdos ntfs ntfscomp hfsplus fat ext2 normal chain boot configfile linux multiboot
