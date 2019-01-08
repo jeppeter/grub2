907be70a05008bf82a18d0aec437d3337aba2283
jeppeter <jeppeter@gmail.com>
2019-01-08 18:16:24 +0800

    to make collect all grub img mod and executable

--- collect.sh
+++ collect.sh
@@ -17,9 +17,11 @@ script_dir=`dirname $script`
 outdir=$script_dir/i386-pc
 if [ $# -gt 0 ]
 	then
-	outdir=shift
+	outdir=$1
+	shift
 fi
 
+echo "outdir [$outdir]"
 if [ ! -d "$outdir" ]
 	then
 	run_cmd mkdir -p "$outdir"
@@ -35,4 +37,13 @@ for _l in $(find $script_dir/grub-core/ -name '*.img')
 do
 	_b=`basename $_l`
 	run_cmd cp "$_l"  "$outdir/$_b"
+done
+
+for _l in $(find $script_dir  -maxdepth 1 -name 'grub-*' )
+do
+	_b=`basename $_l`
+	if [ -f "$_l" ]
+		then
+		run_cmd cp "$_l" "$outdir/$_b"
+	fi
 done
